7bf46d109ee0972c1463e84cb662e85507c2c323
Letícia <161726939+leletis09@users.noreply.github.com>
2024-04-01 10:28:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const searchIcon = document.querySelector(".search-icon");
+const searchForm = document.querySelector(".search-form");
+
+searchIcon.addEventListener('click',() => {
+    searchForm.classList.add("active");
+});
